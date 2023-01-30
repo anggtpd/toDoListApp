6cb8bdb6f199e911bc4a509f1cf42d0eee8ed315
anggtpd <anggitapinasthikadewi@gmail.com>
2023-01-30 21:42:04 +0700

    added a clear text feature

--- lib/main.dart
+++ lib/main.dart
@@ -29,32 +29,31 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  // final TextEditingController newTask = TextEditingController();
+  final TextEditingController newTask = TextEditingController();
   final FirebaseFirestore db = FirebaseFirestore.instance;
   final task = <String, dynamic>{"taskName": "Meeting penting", "value": false};
 
   @override
   Widget build(BuildContext context) {
-    // print(newTask);
     return Scaffold(
       appBar: AppBar(
         title: const Text('To-Do List'),
       ),
       body: Column(
         children: [
-          // Expanded(
-          //   child: ListView(
-          //     children: [],
-          //   ),
-          // ),
           taskList(),
           Padding(
             padding: const EdgeInsets.all(8.0),
             child: TextField(
-              // controller: newTask,
+              controller: newTask,
               decoration: InputDecoration(
-                hintText: 'Add a new task...',
-              ),
+                  hintText: 'Add a new task...',
+                  suffix: GestureDetector(
+                    onTap: () {
+                      newTask.clear();
+                    },
+                    child: const Icon(Icons.clear),
+                  )),
               onSubmitted: (newTask) async {
                 final taskbaru = <String, dynamic>{
                   "taskName": newTask,
